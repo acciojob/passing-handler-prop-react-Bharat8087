83ba88614ba47f1543ac532afe068996b319ce39
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-06-07 10:43:38 +0530

    Create Selection.js

---
+++ src/Selection.js
@@ -0,0 +1,14 @@
+
+import React, { useEffect, useState } from 'react';
+
+const Selection = ({ color }) => {
+  const [bgColor, setBgColor] = useState('');
+
+  useEffect(() => {
+    setBgColor(color);
+  }, [color]);
+
+  return <div className="fix-box" style={{ backgroundColor: bgColor, width: '100px', height: '100px', margin: '10px' }}></div>;
+};
+
+export default Selection;
